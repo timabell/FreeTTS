dea5822ee7a620b5c4ccfe8fe2bcbca9f3530427
lamere <lamere@4963320b-1a4a-0410-81c8-f0a525965860>
2001-12-21 17:20:39 +0000

    Added run.bat files to the demo directories to make it easier to try out FreeTTS on a windows system.
    Added documentation in the demo README.html files about how to run the
    demos on a win32 system.
    
    Added freetts.bat in the bin directory which is the win32 equivalent
    of freetts, the front-end script to run a command line freetts app.

---
+++ bin/freetts.bat
@@ -0,0 +1,1 @@
+java -Xms64m -classpath ..\lib\cmuawb.jar;..\lib\cmukal16.jar;..\lib\cmukal8.jar;..\lib\cmulex.jar;..\lib\freetts.jar;..\lib\jsapi.jar com.sun.speech.freetts.FreeTTS %1 %2 %3 %4
---
+++ demo/JSAPI/MixedVoices/run.bat
@@ -0,0 +1,2 @@
+javac -classpath ..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar -d ..\..\..\classes *.java
+java -cp ...\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar %1
