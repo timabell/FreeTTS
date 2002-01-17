82291731c8d688fb5e6004f070b128ec0361a07f
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-01-17 21:32:58 +0000

    Added more regular expressions.

--- com/sun/speech/freetts/en/us/USEnglish.java
+++ com/sun/speech/freetts/en/us/USEnglish.java
@@ -43,17 +43,45 @@ public class USEnglish {
     public static final String RX_DEFAULT_US_EN_INT = "-?[0-9]+";
     /** default double regular expression pattern */
     public static final String RX_DEFAULT_US_EN_DOUBLE =
-    "-?(([0-9]+\\.[0-9]*)|([0-9]+)|(\\.[0-9]+))([eE][---+]?[0-9]+)?";
+	"-?(([0-9]+\\.[0-9]*)|([0-9]+)|(\\.[0-9]+))([eE][---+]?[0-9]+)?";
     /** default integer with commas  regular expression pattern */
     public static final String RX_DEFAULT_US_EN_COMMAINT =
-    "[0-9][0-9]?[0-9]?,([0-9][0-9][0-9],)*[0-9][0-9][0-9](\\.[0-9]+)?";
+	"[0-9][0-9]?[0-9]?,([0-9][0-9][0-9],)*[0-9][0-9][0-9](\\.[0-9]+)?";
     /** default digits regular expression pattern */
     public static final String RX_DEFAULT_US_EN_DIGITS = "[0-9][0-9]*";
     /** default dotted abbreviation  regular expression pattern */
-    public static final String RX_DEFAULT_US_EN_DOTTED_ABBREV = "([A-Za-z]\\.)*[A-Za-z]";
+    public static final String RX_DEFAULT_US_EN_DOTTED_ABBREV = "([A-Za-z]\\.)+";
     /** default ordinal number regular expression pattern */
     public static final String RX_DEFAULT_US_EN_ORDINAL_NUMBER =
-    "[0-9][0-9,]*(th|TH|st|ST|nd|ND|rd|RD)";
+	"[0-9][0-9,]*(th|TH|st|ST|nd|ND|rd|RD)";
+    /** default has-vowel regular expression */
+    public static final String RX_DEFAULT_HAS_VOWEL = ".*[aeiouAEIOU].*";
+    /** default US money regular expression */
+    public static final String RX_DEFAULT_US_MONEY = "\\$[0-9,]+(\\.[0-9]+)?";
+    /** default -illion regular expression */
+    public static final String RX_DEFAULT_ILLION = ".*illion";
+    /** default digits2dash (e.g. 999-999-999) regular expression */
+    public static final String RX_DEFAULT_DIGITS2DASH = "[0-9]+(-[0-9]+)+";
+    /** default digits/digits (e.g. 999/999) regular expression */
+    public static final String RX_DEFAULT_DIGITSSLASHDIGITS = "[0-9]+/[0-9]+";
+    /** default number time regular expression */
+    public static final String RX_DEFAULT_NUMBER_TIME = "((0[0-2])|(1[0-9])):([0-5][0-9])";
+    /** default Roman numerals regular expression */
+    public static final String RX_DEFAULT_ROMAN_NUMBER =
+	"(II?I?|IV|VI?I?I?|IX|X[VIX]*)";
+    /** default drst "Dr. St" regular expression */
+    public static final String RX_DEFAULT_DRST = "([dD][Rr]|[Ss][Tt])";
+    /** default numess */
+    public static final String RX_DEFAULT_NUMESS = "[0-9]+s";
+    /** default 7-digit phone number */
+    public static final String RX_DEFAULT_SEVEN_DIGIT_PHONE_NUMBER =
+	"[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]";
+    /** default 4-digit number */
+    public static final String RX_DEFAULT_FOUR_DIGIT =
+	"[0-9][0-9][0-9][0-9]";
+    /** default 3-digit number */
+    public static final String RX_DEFAULT_THREE_DIGIT =
+	"[0-9][0-9][0-9]";
     
     
     /** whitespace regular expression pattern */
@@ -80,6 +108,30 @@ public class USEnglish {
     public static String RX_DOTTED_ABBREV = RX_DEFAULT_US_EN_DOTTED_ABBREV;
     /** ordinal number regular expression pattern */
     public static String RX_ORDINAL_NUMBER = RX_DEFAULT_US_EN_ORDINAL_NUMBER;
+    /** has-vowel regular expression */
+    public static final String RX_HAS_VOWEL = RX_DEFAULT_HAS_VOWEL;
+    /** US money regular expression */
+    public static final String RX_US_MONEY = RX_DEFAULT_US_MONEY;
+    /** -illion regular expression */
+    public static final String RX_ILLION = RX_DEFAULT_ILLION;
+    /** digits2dash (e.g. 999-999-999) regular expression */
+    public static final String RX_DIGITS2DASH = RX_DEFAULT_DIGITS2DASH;
+    /** digits/digits (e.g. 999/999) regular expression */
+    public static final String RX_DIGITSSLASHDIGITS = RX_DEFAULT_DIGITSSLASHDIGITS;
+    /** number time regular expression */
+    public static final String RX_NUMBER_TIME = RX_DEFAULT_NUMBER_TIME;
+    /** Roman numerals regular expression */
+    public static final String RX_ROMAN_NUMBER = RX_DEFAULT_ROMAN_NUMBER;
+    /** drst "Dr. St" regular expression */
+    public static final String RX_DRST = RX_DEFAULT_DRST;
+    /** default numess */
+    public static final String RX_NUMESS = RX_DEFAULT_NUMESS;
+    /** 7-digit phone number */
+    public static final String RX_SEVEN_DIGIT_PHONE_NUMBER = RX_DEFAULT_SEVEN_DIGIT_PHONE_NUMBER;
+    /** 4-digit number */
+    public static final String RX_FOUR_DIGIT = RX_DEFAULT_FOUR_DIGIT;
+    /** 3-digit number */
+    public static final String RX_THREE_DIGIT = RX_DEFAULT_THREE_DIGIT;
     
 
     // the following symbols are from lang/usenglish/us_text.c
