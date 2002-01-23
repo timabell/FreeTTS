02b79c7e153191a4a219e089f220ac4e6e3b50ef
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-01-23 16:05:54 +0000

    run.bat: If JAVA_HOME does not exist, it flags an error and exits. If JAVA_HOME exists, it first does a "java -version", and then runs the demo.

--- demo/JSAPI/JTime/run.bat
+++ demo/JSAPI/JTime/run.bat
@@ -1,2 +1,13 @@
-javac -classpath ..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar -d ..\..\..\classes *.java
-java -cp ...\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar %1
+@ECHO off
+
+IF "%JAVA_HOME%"=="" GOTO java_home_missing
+
+%JAVA_HOME%/bin/java -version
+%JAVA_HOME%/bin/java -cp ..\..\..\lib\demo.jar;..\..\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar JTime
+GOTO end
+
+:java_home_missing
+ECHO Error: JAVA_HOME environment variable is not defined.
+
+:end
+@ECHO on
