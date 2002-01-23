2c970f247bbfcb75b09a742df6f356d9ee281340
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-01-23 17:50:40 +0000

    Added batch files for running the Client/Server demo just from the DOS command prompt.

---
+++ demo/freetts/ClientServer/runserver.bat
@@ -0,0 +1,13 @@
+@ECHO off
+
+IF "%JAVA_HOME%"=="" GOTO java_home_missing
+
+%JAVA_HOME%/bin/java -version
+%JAVA_HOME%/bin/java -cp ..\..\..\lib\demo.jar;..\..\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar -Dmetrics=false Server
+GOTO end
+
+:java_home_missing
+ECHO Error: JAVA_HOME environment variable is not defined.
+
+:end
+@ECHO on
