d7aaedbfb1afbd107f5bce866f692679b6daf43c
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-03-13 20:42:16 +0000

    Added "-Dserver -Dport" to alert the users that they can set the host/port, just like the Makefiles.

--- demo/freetts/ClientServer/runserver.bat
+++ demo/freetts/ClientServer/runserver.bat
@@ -3,7 +3,7 @@
 IF "%JAVA_HOME%"=="" GOTO java_home_missing
 
 %JAVA_HOME%\bin\java -version
-%JAVA_HOME%\bin\java -cp ..\..\..\lib\demo.jar;..\..\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar -Dmetrics=false Server
+%JAVA_HOME%\bin\java -cp ..\..\..\lib\demo.jar;..\..\..\classes;..\..\..\lib\cmuawb.jar;..\..\..\lib\cmukal16.jar;..\..\..\lib\cmukal8.jar;..\..\..\lib\cmulex.jar;..\..\..\lib\cmutimelex.jar;..\..\..\lib\freetts.jar;..\..\..\lib\jsapi.jar -Dmetrics=false -Dserver=localhost -Dport=1111 Server
 GOTO end
 
 :java_home_missing
