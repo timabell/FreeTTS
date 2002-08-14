52bab181e9eab54df39ec52332dbc5bbd63e3ec5
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-08-14 18:58:10 +0000

    Added the code for the WebStart clock.

---
+++ demo/JSAPI/WebStartClock/JSAPIClock.java
@@ -0,0 +1,79 @@
+import com.sun.speech.freetts.jsapi.FreeTTSEngineCentral; 
+import com.sun.speech.freetts.jsapi.FreeTTSSynthesizerModeDesc;
+
+import java.util.Locale;
+
+import javax.speech.EngineList; 
+import javax.speech.EngineCreate; 
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+
+/**
+ * A talking clock powered by FreeTTS.
+ */
+public class JSAPIClock extends Clock {
+
+    protected Synthesizer synthesizer;
+
+
+    /**
+     * Creates the synthesizer, called by the constructor.
+     */
+    public void createSynthesizer() {
+
+        String synthesizerName =
+            "Limited domain FreeTTS Speech Synthesizer from Sun Labs";
+        String lexiconName =
+            "com.sun.speech.freetts.en.us.CMULexicon";
+        String playerName =
+            "com.sun.speech.freetts.audio.JavaClipAudioPlayer";
+
+        try {
+            SynthesizerModeDesc desc = 
+                new FreeTTSSynthesizerModeDesc(synthesizerName, 
+                                               null,
+                                               Locale.US, 
+                                               lexiconName,
+                                               playerName);
+
+            FreeTTSEngineCentral central = new FreeTTSEngineCentral();
+            EngineList list = central.createEngineList(desc); 
+            
+            if (list.size() > 0) { 
+                EngineCreate creator = (EngineCreate) list.get(0); 
+                synthesizer = (Synthesizer) creator.createEngine(); 
+            } 
+            if (synthesizer == null) {
+                System.err.println("Cannot create synthesizer");
+                System.exit(1);
+            }
+            synthesizer.allocate();
+            synthesizer.resume();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Speaks the given time in full text.
+     *
+     * @param time time in full text
+     */
+    protected void speak(String time) {
+        synthesizer.speakPlainText(time, null);
+    }
+
+
+    /**
+     * main() method to run the WebStartClock.
+     */
+    public static void main(String args[]) {
+        Clock frame = new JSAPIClock();
+        frame.pack();
+        frame.show();
+        frame.createSynthesizer();
+        frame.startClock();
+    }
+}
