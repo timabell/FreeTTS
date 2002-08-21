a78052db041b7d8e7c842e84be8dd6961e477036
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-08-21 17:36:21 +0000

    Added "Portions copyright, Sun Microsystems..."

--- de/dfki/lt/freetts/en/us/MbrolaVoiceUS3.java
+++ de/dfki/lt/freetts/en/us/MbrolaVoiceUS3.java
@@ -1,5 +1,6 @@
 /**
  * Copyright 2002 DFKI GmbH.
+ * Portions Copyright 2002 Sun Microsystems, Inc.
  * All Rights Reserved.  Use is subject to license terms.
  *
  * See the file "license.terms" for information on usage and
