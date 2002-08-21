0576eb25875fd4f2cf6a1602b140968eb6716ba7
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2002-08-21 17:40:29 +0000

    Adding Validator classes.

---
+++ de/dfki/lt/freetts/en/us/MbrolaVoiceUS1Validator.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2002 DFKI GmbH.
+ * Portions Copyright 2002 Sun Microsystems, Inc.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ */
+
+package de.dfki.lt.freetts.en.us;
+
+/**
+ * Validates that MbrolaVoiceUS1 is installed and available.
+ */
+public class MbrolaVoiceUS1Validator extends MbrolaVoiceValidator {
+
+    public MbrolaVoiceUS1Validator() {
+        super((new MbrolaVoiceUS1()));
+    }
+}
---
+++ de/dfki/lt/freetts/en/us/MbrolaVoiceValidator.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2002 DFKI GmbH.
+ * Portions Copyright 2002 Sun Microsystems, Inc.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ */
+
+package de.dfki.lt.freetts.en.us;
+
+import com.sun.speech.freetts.Validator;
+
+import java.io.File;
+
+/**
+ * Shows this MbrolaVoice is valid (or usable). It tests for 
+ * the following:
+ * 
+ * <ol>
+ * <li> Check that the "mbrola.base" System property is defined,
+ *      and that directory exists.
+ * <li> Check that the $(mbrola.base)/mbrola binary exists.
+ * <li> Check that the transition table exists. It is assumed 
+ *      to be at $(mbrola.base)/us1/us1mrpa.
+ * <li> Check that its voice database exists.
+ * </ol>
+ */
+public class MbrolaVoiceValidator implements Validator {
+
+    private MbrolaVoice mbrolaVoice;
+
+    public MbrolaVoiceValidator(MbrolaVoice mbrolaVoice) {
+        this.mbrolaVoice = mbrolaVoice;
+    }
+
+    /**
+     * Returns true if the MbrolaVoice tested is valid.
+     */
+    public boolean isValid() {
+        String mbrolaBase = System.getProperty("mbrola.base");
+        File mbrolaBinary = new File(mbrolaVoice.getMbrolaBinary());
+        File mbrolaRenameTable = new File(mbrolaVoice.getRenameTable());
+        File mbrolaVoiceDB = new File(mbrolaVoice.getDatabase());
+
+        if (mbrolaBase == null) {
+            System.err.println
+                (toString() +
+                 ": System property \"mbrola.base\" is undefined.");
+            return false;
+        }
+        if (!mbrolaBinary.exists()) {
+            System.err.println(toString() + ": MBROLA binary does not exist");
+            return false;
+        }
+        if (!mbrolaRenameTable.exists()) {
+            System.err.println(toString() +
+                               ": MBROLA rename table does not exist");
+            return false;
+        }
+        if (!mbrolaVoiceDB.exists()) {
+            System.err.println(toString() + ": voice database does not exist");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the name of this validator.
+     *
+     * @return the name of this Validator
+     */
+    public String toString() {
+        return (mbrolaVoice.toString() + "Validator");
+    }
+}
+
