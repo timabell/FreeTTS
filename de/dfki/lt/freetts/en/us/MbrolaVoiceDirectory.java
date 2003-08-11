6c4078d6997549484fa22f21a0cd7a73743a8f61
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2003-08-11 17:56:49 +0000

    Gave the MBROLA voices appropriate domain ('general') and organization ('mbrola').

--- de/dfki/lt/freetts/en/us/MbrolaVoiceDirectory.java
+++ de/dfki/lt/freetts/en/us/MbrolaVoiceDirectory.java
@@ -10,16 +10,18 @@ import com.sun.speech.freetts.ValidationException;
 
 //TODO documentation
 public class MbrolaVoiceDirectory extends VoiceDirectory {
+    
     public Voice[] getVoices() {
+    
         Voice mbrola1 = new MbrolaVoice(true, "us1", "us1", 150f, 180F, 22F,
                 "mbrola1", Gender.FEMALE, Age.YOUNGER_ADULT, "MBROLA Voice 1",
-                Locale.US, "TEST/TODO", "TEST/TODO");
+                Locale.US, "general", "mbrola");
         Voice mbrola2 = new MbrolaVoice(true, "us2", "us2", 150f, 115F, 12F,
                 "mbrola2", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 2",
-                Locale.US, "TEST/TODO", "TEST/TODO");
+                Locale.US, "general", "mbrola");
         Voice mbrola3 = new MbrolaVoice(true, "us3", "us3", 150f, 125F, 12F,
                 "mbrola3", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 3",
-                Locale.US, "TEST/TODO", "TEST/TODO");
+                Locale.US, "general", "mbrola");
 
         Voice[] voices = {mbrola1, mbrola2, mbrola3};
 
