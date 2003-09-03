c3a6ecfe1817b55ae98fc990213a2a0cb937a266
ppk96 <ppk96@4963320b-1a4a-0410-81c8-f0a525965860>
2003-09-03 14:23:47 +0000

    Fixed MbrolaVoiceDirectory so that it checks for a null or empty string for the system property "mbrola.base".

--- de/dfki/lt/freetts/en/us/MbrolaVoiceDirectory.java
+++ de/dfki/lt/freetts/en/us/MbrolaVoiceDirectory.java
@@ -4,6 +4,10 @@ import com.sun.speech.freetts.VoiceDirectory;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.Gender;
 import com.sun.speech.freetts.Age;
+
+import com.sun.speech.freetts.util.Utilities;
+
+import java.util.ArrayList;
 import java.util.Locale;
 
 import com.sun.speech.freetts.ValidationException;
@@ -12,36 +16,54 @@ import com.sun.speech.freetts.ValidationException;
 public class MbrolaVoiceDirectory extends VoiceDirectory {
     
     public Voice[] getVoices() {
-    
-        Voice mbrola1 = new MbrolaVoice(true, "us1", "us1", 150f, 180F, 22F,
-                "mbrola1", Gender.FEMALE, Age.YOUNGER_ADULT, "MBROLA Voice 1",
-                Locale.US, "general", "mbrola");
-        Voice mbrola2 = new MbrolaVoice(true, "us2", "us2", 150f, 115F, 12F,
-                "mbrola2", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 2",
-                Locale.US, "general", "mbrola");
-        Voice mbrola3 = new MbrolaVoice(true, "us3", "us3", 150f, 125F, 12F,
-                "mbrola3", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 3",
-                Locale.US, "general", "mbrola");
 
-        Voice[] voices = {mbrola1, mbrola2, mbrola3};
+        String base = Utilities.getProperty("mbrola.base", null);
 
-        for (int i = 0; i < voices.length; i++) {
-            if (voices[i] == null) {
-                throw new Error("Error instantiating MBROLA voice");
+        if (base == null || base.trim().length() == 0) {
+            throw new Error
+                ("System property \"mbrola.base\" is undefined. " +
+                 "You might need to set the MBROLA_DIR environment variable.");
+        } else {
+            
+            Voice mbrola1 = new MbrolaVoice
+                (true, "us1", "us1", 150f, 180F, 22F,
+                 "mbrola1", Gender.FEMALE, Age.YOUNGER_ADULT, "MBROLA Voice 1",
+                 Locale.US, "general", "mbrola");
+
+            Voice mbrola2 = new MbrolaVoice
+                (true, "us2", "us2", 150f, 115F, 12F,
+                 "mbrola2", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 2",
+                 Locale.US, "general", "mbrola");
+
+            Voice mbrola3 = new MbrolaVoice
+                (true, "us3", "us3", 150f, 125F, 12F,
+                 "mbrola3", Gender.MALE, Age.YOUNGER_ADULT, "MBROLA Voice 3",
+                 Locale.US, "general", "mbrola");
+
+            Voice[] voices = {mbrola1, mbrola2, mbrola3};
+            
+            ArrayList validVoices = new ArrayList();
+            int count = 0;
+
+            for (int i = 0; i < voices.length; i++) {
+                MbrolaVoiceValidator validator = 
+                    new MbrolaVoiceValidator((MbrolaVoice) voices[i]);
+                try {
+                    validator.validate();
+                    validVoices.add(voices[i]);
+                    count++;
+                } catch (ValidationException ve) {
+                    // does nothing if the voice is not found 
+                }
             }
-            MbrolaVoiceValidator validator = new
-                MbrolaVoiceValidator((MbrolaVoice) voices[i]);
-            try {
-                validator.validate();
-            } catch (ValidationException ve) {
-                throw new Error("Problem starting MBROLA voice: " + ve);
-            }
+
+            return ((Voice[])validVoices.toArray(new Voice[count]));
         }
-
-        return voices;
     }
 
     public static void main(String[] args) {
         System.out.println((new MbrolaVoiceDirectory()).toString());
     }
 }
+
+
