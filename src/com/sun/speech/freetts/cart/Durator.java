a181d3022aefe8c551ec33bb53ec173338780b8d
schnelle <schnelle@4963320b-1a4a-0410-81c8-f0a525965860>
2011-12-02 20:48:54 +0000

    Style corrections.

--- src/com/sun/speech/freetts/cart/Durator.java
+++ src/com/sun/speech/freetts/cart/Durator.java
@@ -21,41 +21,41 @@ import com.sun.speech.freetts.Utterance;
 import com.sun.speech.freetts.UtteranceProcessor;
 
 /**
- * Determines duration timing for
- * <code>Relation.SEGMENT</code> relations in an utterance.
- * Annotates the <code>Relation.SEGMENT</code> relation with an
- * "end" time feature.
- *
- * <p>[[[TODO: The mean words-per-minute rate should become part
- * of the CART data.  For now, it is passed into the constructor.]]]
- *
+ * Determines duration timing for <code>Relation.SEGMENT</code> relations in an
+ * utterance. Annotates the <code>Relation.SEGMENT</code> relation with an "end"
+ * time feature.
+ * 
+ * <p>
+ * [[[TODO: The mean words-per-minute rate should become part of the CART data.
+ * For now, it is passed into the constructor.]]]
+ * 
  * @see Relation#SEGMENT
  */
 public class Durator implements UtteranceProcessor {
     /**
-     * The CART used for this duration UtteranceProcessor.  It is
-     * passed into the constructor.
+     * The CART used for this duration UtteranceProcessor. It is passed into the
+     * constructor.
      */
     protected final CART cart;
 
     /**
-     * The PhoneDurations used for this duration UtteranceProcessor.
-     * It is passed into the constructor.
+     * The PhoneDurations used for this duration UtteranceProcessor. It is
+     * passed into the constructor.
      */
     protected final PhoneDurations durations;
 
+    private static final PathExtractor DURATION_STRETCH_PATH = new PathExtractorImpl(
+            "R:SylStructure.parent.parent.R:Token.parent.local_duration_stretch",
+            true);
 
-    private static final PathExtractor DURATION_STRETCH_PATH  =
-	new PathExtractorImpl(
-        "R:SylStructure.parent.parent.R:Token.parent.local_duration_stretch", 
-	true);
-    
     /**
-     * Creates a new duration UtteranceProcessor with the given
-     * CART and phone durations.
-     *
-     * @param cart contains zscore duration data
-     * @param durations contains mean and standard deviation phone durations
+     * Creates a new duration UtteranceProcessor with the given CART and phone
+     * durations.
+     * 
+     * @param cart
+     *            contains zscore duration data
+     * @param durations
+     *            contains mean and standard deviation phone durations
      */
     public Durator(CART cart, PhoneDurations durations) {
         this.cart = cart;
@@ -63,18 +63,18 @@ public class Durator implements UtteranceProcessor {
     }
 
     /**
-     * Annotates the <code>Relation.SEGMENT</code> relations with
-     * cumulative "end" time
-     * features based on phone durations.  Expects the CART to return
+     * Annotates the <code>Relation.SEGMENT</code> relations with cumulative
+     * "end" time features based on phone durations. Expects the CART to return
      * a zscore for each phone, which specifies the number of standard
-     * deviations from the mean.  This is coupled with a phone
-     * durations table that returns the mean and standard deviation
-     * for phones.
+     * deviations from the mean. This is coupled with a phone durations table
+     * that returns the mean and standard deviation for phones.
      * 
-     * @param  utterance the utterance to process
-     *
-     * @throws ProcessException if a problem is encountered during the
-     *         processing of the utterance
+     * @param utterance
+     *            the utterance to process
+     * 
+     * @throws ProcessException
+     *             if a problem is encountered during the processing of the
+     *             utterance
      */
     public void processUtterance(Utterance utterance) throws ProcessException {
         PhoneDuration durStat;
@@ -82,30 +82,30 @@ public class Durator implements UtteranceProcessor {
         float zdur;
         float dur;
         float end = 0.0f;
-	float localDurationStretch;
+        float localDurationStretch;
 
         // Go through each of the segments and calculate a duration
-        // for it.  Store the cumulative end time for the duration in
+        // for it. Store the cumulative end time for the duration in
         // the "end" feature of the segment.
         //
-	for (Item segment =
-                 utterance.getRelation(Relation.SEGMENT).getHead();
-		segment != null; segment = segment.getNext()) {
+        for (Item segment = utterance.getRelation(Relation.SEGMENT).getHead(); segment != null; segment = segment
+                .getNext()) {
             zdur = ((Float) cart.interpret(segment)).floatValue();
-            durStat = durations.getPhoneDuration(
-                segment.getFeatures().getString("name"));
+            durStat = durations.getPhoneDuration(segment.getFeatures()
+                    .getString("name"));
 
-	    Object tval = DURATION_STRETCH_PATH.findFeature(segment);
-	    localDurationStretch = Float.parseFloat(tval.toString());
+            Object tval = DURATION_STRETCH_PATH.findFeature(segment);
+            localDurationStretch = Float.parseFloat(tval.toString());
 
-	    if (localDurationStretch == 0.0) {
-		localDurationStretch = durationStretch;
-	    } else {
-		localDurationStretch *= durationStretch;
-	    }
+            if (localDurationStretch == 0.0) {
+                localDurationStretch = durationStretch;
+            } else {
+                localDurationStretch *= durationStretch;
+            }
 
-            dur = localDurationStretch * ((zdur*durStat.getStandardDeviation())
-                                 + durStat.getMean());
+            dur = localDurationStretch
+                    * ((zdur * durStat.getStandardDeviation()) + durStat
+                            .getMean());
             end += dur;
             segment.getFeatures().setFloat("end", end);
         }
